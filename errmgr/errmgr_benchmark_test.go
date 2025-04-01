39a15822bb49b8fe2b331196ad41d03ef5ed9e56
oleku konko <oleku.konko@gmail.com>
2025-04-01 17:02:51 +0100

    add becnhmark

--- errmgr/errmgr_benchmark_test.go
+++ errmgr/errmgr_benchmark_test.go
@@ -2,14 +2,57 @@ package errmgr
 
 import (
 	"fmt"
+	"github.com/olekukonko/errors"
 	"testing"
 )
 
-// BenchmarkTemplateError measures templated error performance.
+// BenchmarkTemplateError measures the performance of creating templated errors.
 func BenchmarkTemplateError(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		err := ErrDBConnection(fmt.Sprintf("connection failed %d", i))
 		err.Free()
+	}
+}
+
+// BenchmarkCodedError measures the performance of creating coded errors.
+func BenchmarkCodedError(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := ErrValidationFailed(fmt.Sprintf("field %d", i))
+		err.Free()
+	}
+}
+
+// BenchmarkCategorizedError measures the performance of creating categorized errors.
+func BenchmarkCategorizedError(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := NetworkError(fmt.Sprintf("host %d", i))
+		err.Free()
+	}
+}
+
+// BenchmarkCallableError measures the performance of creating custom callable errors.
+func BenchmarkCallableError(b *testing.B) {
+	fn := Callable("custom", func(args ...interface{}) *errors.Error {
+		return errors.New(fmt.Sprintf("custom %v", args[0]))
+	})
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := fn(i)
+		err.Free()
+	}
+}
+
+// BenchmarkMetrics measures the performance of retrieving error metrics.
+func BenchmarkMetrics(b *testing.B) {
+	for i := 0; i < 100; i++ {
+		err := ErrDBConnection(fmt.Sprintf("test %d", i))
+		err.Free()
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = Metrics()
 	}
 }
